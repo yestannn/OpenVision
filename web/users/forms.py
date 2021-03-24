8a75cf31516e62bb01ba2153ff4d6f4a1d6f329f
yestannn <yestan3141@gmail.com>
2021-03-24 19:58:33 +0600

    profile and user update forms

--- web/users/forms.py
+++ web/users/forms.py
@@ -21,3 +21,9 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email' , 'first_name','last_name' ]
         
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['company' ,]
--- web/users/models.py
+++ web/users/models.py
@@ -6,5 +6,11 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	company = models.CharField(default='',max_length = 100)
 
+	def __str__(self):
+		return f'{self.user.username} Profile'
+
+	def save(self, *args, **kwargs):
+		super(Profile, self).save(*args, **kwargs)
 	
