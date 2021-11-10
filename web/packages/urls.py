b751d3e03379ec44d6a4af0d8f56f39bc53b7d4f
yestannn <55324492+yestannn@users.noreply.github.com>
2021-11-10 17:04:50 +0600

    Delete web directory

--- web/packages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.decorators.http import  require_POST
-from . import views
-
-
-urlpatterns = [
-    path('', views.home, name='packages-home'),
-]
--- web/packages/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    context = {
-        'post': 'Home page'
-        }
-
-    return render(request, 'packages/home.html', context)
